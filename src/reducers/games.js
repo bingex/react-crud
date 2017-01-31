aaccd92c8c1e8ccc54f1470c536207ef1a5536b5
bingex <romandatsyuk@gmail.com>
2017-01-31 23:37:56 +0200

    ExpressJS Server. AJAX Request.

--- src/reducers/games.js
+++ src/reducers/games.js
@@ -1,5 +1,11 @@
+import { SET_GAMES } from '../actions'
+
 export default function games(state = [], action = {}) {
+  console.log(action)
   switch (action.type) {
+    case SET_GAMES:
+      return action.games
+
     default: return state
   }
 }
